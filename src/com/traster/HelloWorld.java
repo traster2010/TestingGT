2b37b31e2490a4deb87fd57ab5ab398cf94236f1
traster2011 <traster2011@gmail.com>
2018-07-02 15:00:46 +0300

    HelloWord  print 123456

--- src/com/traster/HelloWorld.java
+++ src/com/traster/HelloWorld.java
@@ -48,7 +48,7 @@ public class HelloWorld {
     public static void main(String[] args) throws Exception {
 
 
-        System.out.println("12345");
+        System.out.println("123456");
         //   --------------------------
         if (true) return;
 
